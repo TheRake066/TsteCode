a5a8120610b2007a8003d2b2eb98de9cc96368b8
The Rake Dev <sidneyvr62@gmail.com>
2025-11-19 14:57:33 -0300

    Delete style.py

--- style.py
+++
@@ -1,8 +0,0 @@
-from InquirerPy import prompt
-
-result = prompt(
-    {"message": "Confirm order?", "type": "confirm", "default": False},
-    style={"questionmark": "#ff9d00 bold"},
-    vi_mode=True,
-    style_override=False,
-)
